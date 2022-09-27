0394b9c7d49352dac44fc8f28d99b6b691430f9b
Brandon Lam <brandonlam4237@gmail.com>
2022-09-27 16:22:13 -0400

    notes can be deleted

--- client/src/components/NoteCard.js
+++ client/src/components/NoteCard.js
@@ -1,7 +1,32 @@
+import "../assets/scss/notecard.scss";
+import trash from "../assets/icons/trash.png";
+import { useNotesContext } from "../hooks/useNotesContext";
+
 function NoteCard({ note }) {
+  const { dispatch } = useNotesContext();
+
+  const handleDelete = async () => {
+    const response = await fetch(
+      "http://localhost:3001/api/notes/" + note._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_NOTE", payload: json });
+    }
+  };
+
   return (
     <div className="note">
-      <div className="title">{note.title}</div>
+      <div className="header">
+        <div className="title">{note.title}</div>
+        <div className="options">
+          <img src={trash} onClick={handleDelete} />
+        </div>
+      </div>
       <div className="content">{note.content}</div>
     </div>
   );
