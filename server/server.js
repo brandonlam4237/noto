b868f55b5cc0853c41124747c0978890b8b9f9ad
Brandon Lam <brandonlam4237@gmail.com>
2022-10-25 20:33:52 -0400

    user model, basic routes, and controller

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const notoRoutes = require("./routes/notes.js");
+const userRoutes = require("./routes/user.js");
 const cors = require("cors");
 
 const app = express();
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/notes", notoRoutes);
+app.use("/api/user", userRoutes);
 
 mongoose
   .connect(uri)
