72127b6f0293a6c4b0721365fd35c21bf74cc46d
Brandon Lam <brandonlam4237@gmail.com>
2022-12-26 15:54:09 -0500

    json web tokens auth for server signup/login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,20 @@
 const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "7d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ msg: "login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
@@ -10,7 +22,8 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
