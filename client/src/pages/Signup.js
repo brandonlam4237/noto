5a4fa1782e41d6698e9b52960565eff1e5469cd3
Brandon Lam <brandonlam4237@gmail.com>
2022-12-26 21:02:05 -0500

    styled signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../assets/scss/signup.scss";
+import logo from "../assets/icons/logo.jpg";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -11,23 +14,35 @@ function Signup() {
 
   return (
     <form className="signup" onSubmit={handleSubmit}>
-      <h3>Sign up</h3>
+      <div className="box">
+        <div className="header">
+          <img src={logo} className="logo" alt="logo" />
+          <h2>NOTO</h2>
+          <div className="slogan">NOTABLY MINIMAL NOTES.</div>
+        </div>
 
-      <label>Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
+        <div className="form-fields">
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="EMAIL ADDRESS"
+          />
 
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="PASSWORD"
+          />
 
-      <button>Sign up</button>
+          <button>SIGNUP</button>
+          <div className="footer">
+            <div>ALREADY HAVE AN ACCOUNT?</div>
+            <Link to="/login">LOGIN TO ACCOUNT</Link>
+          </div>
+        </div>
+      </div>
     </form>
   );
 }
